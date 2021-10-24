12766bdf3cd550f67d2ddc8bcd8545033dcf7f7a
William Araujo <w.teleco@gmail.com>
2021-10-24 20:04:04 -0300

    aula4

--- app/domain/account/account_model.py
+++ app/domain/account/account_model.py
@@ -1,6 +1,8 @@
 from config.database import Base
 from sqlalchemy.types import Date
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
+from domain.transaction.transaction_model import Transaction
 
 
 class Account(Base):
@@ -12,6 +14,7 @@ class Account(Base):
     document = Column(String, nullable=False)
     email = Column(String, nullable=False)
     phone_number = Column(String, nullable=False)
+    transaction = relationship(Transaction, backref="accounts")
 
     def __repr__(self) -> str:
         return f"{self.name}, {self.born_date}, {self.document}, {self.email}, {self.phone_number}"
--- app/domain/transaction/transaction_model.py
+++ app/domain/transaction/transaction_model.py
@@ -1,6 +1,9 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 from config.database import Base
-from sqlalchemy.types import Date
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy import (Column, Integer, String, 
+                        Float, ForeignKey)
 
 
 class Transaction(Base):
@@ -9,6 +12,8 @@ class Transaction(Base):
     id = Column(Integer, primary_key=True, index=True)
     transaction_type = Column(String, nullable=False)
     amount = Column(Float, nullable=False)
+    account_id = Column(Integer, ForeignKey("account_id"))
+    account = relationship("Account")
 
     def __repr__(self) -> str:
         return f"{self.transaction_type}, {self.amount}"
